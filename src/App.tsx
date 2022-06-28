1a99f0ddea6c1d2ab4c29bb7a704239a6f60736f
Catalin Andrei Preda <catalinandreipreda@outlook.com>
2022-06-28 10:53:47 +0100

    Initial game UI

---
+++ src/App.tsx
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react'
+import Square from './Square'
+
+const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""]
+const INITIAL_SCORES = {X:0, O:0}
+const WIN_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+
+function App() {
+  const handleCellClick = (event:any) =>{
+    /**
+     * Updates the gameState when a cell is clicked.
+     * This allows the game to render a new X or O when a cell is clicked
+     */
+
+    // Getting the index of the clicked cell
+    const cellIndex = Number(event.currentTarget.getAttribute('data-cell-index'))
+    // Getting the current value of that cell
+    const currentValue = gameState[cellIndex]
+    // Exit the method if the cell already has a value
+    if(currentValue){
+      return
+    }
+    // Updating the state (without mutating the current one)
+    const newValues = [...gameState]
+    newValues[cellIndex] = currentPlayer
+    setGameState(newValues)
+    
+  }
+
+  const changePlayer = () => {
+    /**
+     * Switching the current player
+     */
+     setCurrentPlayer(currentPlayer ==='X' ? 'O' : 'X')
+  }
+
+  const checkForWinner = () =>{
+    /**
+     * Checks if the currentPlayer won after the last move.
+     */
+    let roundWon = false
+
+    // Checking each possible winning combination
+    WIN_COMBOS.map(winCombo => {
+      // For every line in WIN_COMBOS, construct a string with 
+      // the values in the gameState
+      const currentLine = gameState[winCombo[0]] + gameState[winCombo[1]] + gameState[winCombo[2]]
+
+      // Check for winning condition
+      if( currentLine === 'XXX' || currentLine === 'OOO')
+        roundWon = true
+    })
+
+    // Handle Win scenarios
+    if(roundWon){
+      handleWin()
+      return
+    }
+    // Handle Draw scenarios
+    if(!gameState.includes("")){
+      handleDraw()
+      return
+    }
+
+    changePlayer()
+
+  
+  }
+
+  const handleWin = () =>{
+    /** Displaying message to winner and reseting the game */
+    alert(`Player ${currentPlayer} won! Congratulations!`)
+    setGameState(INITIAL_GAME_STATE)
+  }
+  const handleDraw = () =>{
+    /** Displaying draw message and reseting the game */
+    alert("It's a draw! Well played everyone")
+    setGameState(INITIAL_GAME_STATE)
+  }
+  // State
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE)
+  const [currentPlayer, setCurrentPlayer] = useState('X')
+  const [scores, setScores] = useState(INITIAL_SCORES)
+
+  // Side effects
+  useEffect(()=>{
+    /**
+     * 
+     */
+    checkForWinner()
+  }, [gameState])
+
+  return (
+    <div className='h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500'>
+      <h1 className='text-center text-5xl mb-4 font-display text-white'>Tic Tac Toe</h1>
+      
+      <div className='grid grid-cols-3 gap-3 mx-auto w-96'>
+        {gameState.map((player, index) => (
+           <Square 
+           key={index} 
+           index = {index}
+           player={player}
+           onClick= {handleCellClick}
+            />
+           
+        ))}
+      </div>
+      <div>Scores</div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Square.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react'
+
+type Props = {
+    index: number
+    onClick(event:any): void
+    player?: string
+}
+
+export default function Square({index, onClick, player}: Props) {
+    const scale = player ? "scale-100" : "scale-0"
+    const textColor = player === "X" ? "text-yellow-200" : "text-fuchsia-300"
+    const hoverStyle = "transition duration-500 hover:scale-105 transform"
+    return (
+        <div   
+            data-cell-index = {index}
+            {...{onClick}}     
+            className= {`h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer ${hoverStyle}`}
+        >
+            <span 
+            className={`transform transition-all dration-150 ease-out ${scale} ${textColor}`}>{player}</span>
+        </div>
+    )
+}
